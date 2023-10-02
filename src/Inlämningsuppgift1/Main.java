a9f1c42a9d10ff7347af3eccc6aae5282a598c6e
Mattias Seth <mattias.seth@gmail.com>
2023-10-02 16:29:37 +0200

    Funkar.

--- src/Inlämningsuppgift1/Main.java
+++ src/Inlämningsuppgift1/Main.java
@@ -2,24 +2,68 @@ package Inlämningsuppgift1;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     // arraylist för växter
+    ArrayList<Växter> växtArray = new ArrayList<>();
+    Scanner userInput = new Scanner(System.in);
+    public String input () {
+        System.out.println("Vilken växt ska få vätska?");
+        String input = userInput.nextLine();
 
+        return input;
+    }
 
-    public static void main(String[] args) {
-
+    public Main (){
         Växter igge = new Kaktus("Igge", 0.2);
         Växter laura = new Palm("Laura", 5);
         Växter meatloaf = new Köttätande("Meatloaf", 0.7);
         Växter olof = new Palm("Olof", 1);
 
-        //System.out.println(olof.getName());
+        växtArray.add(igge);
+        växtArray.add(laura);
+        växtArray.add(meatloaf);
+        växtArray.add(olof);
 
+        String inputData = input();
+        boolean nameCheck = false;
+
+        for (int i=0; i<växtArray.size(); i++){
+            if(växtArray.get(i).getName().equalsIgnoreCase(inputData)){
+                växtArray.get(i).printMe();
+                nameCheck = true;
+                break;
+            }
+        }
+
+        if (nameCheck == false){
+            System.out.println("Fel namn på växten");
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        /*
+        Växter igge = new Kaktus("Igge", 0.2);
+        Växter laura = new Palm("Laura", 5);
+        Växter meatloaf = new Köttätande("Meatloaf", 0.7);
+        Växter olof = new Palm("Olof", 1);
+        */
+
+
+        /*
         igge.printMe();
         olof.printMe();
         laura.printMe();
         meatloaf.printMe();
+        */
+
+        Main m = new Main();
+        //m.input();
 
     }
 }
